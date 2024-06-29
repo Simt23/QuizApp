ff9d8f922194f588fd1680c67354168232130a16
simran tomar <112646002+Simt23@users.noreply.github.com>
2024-06-29 23:40:48 +0530

    Update QuizResult.js

--- src/Components/QuizResult.js
+++ src/Components/QuizResult.js
@@ -7,7 +7,7 @@
      Score:  {props.score}<br/>
      Total Score: {props.totalScore}
     </div>
-    <button id="next-button" onClick={props.tryAgain}>Try Again</button>
+    <button id="next-button" onClick={props.tryAgain}>Try Again!</button>
     </>
    
   )
